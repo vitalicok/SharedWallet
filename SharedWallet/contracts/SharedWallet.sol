afc2d319dda4ed298aeb96439ccbc7c6ba9e13a1
Vitaliy Shal <vitalicok@vitaliys-air.home>
2021-08-16 01:27:09 +0200

    add new class Allowance to split off the functionalities and adding some events to track data

--- SharedWallet/contracts/SharedWallet.sol
+++ SharedWallet/contracts/SharedWallet.sol
@@ -4,10 +4,23 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import  "./utils/OwnerExtension.sol";
 
 /*
-Main contract module which is responsible for everything
-that is related to Wallet functionalities
+Contract module that serves for storing data
+of a key-pair user-amount with a possibility 
+to add and remove money to a specific user
 */
-contract SharedWallet is OwnerExtension{
+contract Allowance is OwnerExtension {
+    
+    /*
+    An event that allows to write down the 
+    information of transaction status along
+    with a person from who this transaction
+    has been performed with a person for who
+    this money is sent, _oldAmount represents
+    and old amount of money whereas _newAmount
+    says how much amount has incresed so the difference
+    between them shows what is the amount that was spent
+    */
+    event AllowanceChanged(address indexed _forWho, address indexed _fromWho, uint _oldAmount, uint _newAmount);
     
     /**
     Variable that collects all the parties that will 
@@ -18,10 +31,12 @@ contract SharedWallet is OwnerExtension{
     mapping(address => uint) public allowance;
     
     /**
-    Method allows to add new users to the hash-map table,
+    Method allows to add a certain amount of money
+    to a specific user to the hash-map table,
     in this case to "allowance" mapper
     */
     function addAllowance(address _who, uint _amount) public {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
         allowance[_who] = _amount;
     }
     
@@ -29,10 +44,34 @@ contract SharedWallet is OwnerExtension{
     Modifier that encapsulates requirements logic
     */
     modifier ownerOrAllowed(uint _amount) {
-        require(isOwner() || allowance[msg.sender] > _amount, "You are not allowed");
+        require(isOwner() || allowance[msg.sender] >= _amount, "You are not allowed");
         _;
     }
     
+    /**
+    Method allows to remove a certain amount of money
+    given to a specific user
+    */
+    function reduceAllowance(address _who, uint _amount) internal {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
+        allowance[_who] -= _amount;
+    }
+}
+
+/*
+Main contract module which is responsible for everything
+that is related to Wallet functionalities and inherits
+contract Allowance
+*/
+contract SharedWallet is Allowance{
+    
+    /*
+    Events record the data of the amount of money 
+    that has been either withdrawn or deposited 
+    to a certain account respectively
+    */
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
     
     /*
     Method that serves for withdrawing money and checking out 
@@ -40,8 +79,19 @@ contract SharedWallet is OwnerExtension{
     who started the transaction
     */
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed {
+        require(_amount <= address(this).balance, "There are not enough funds stored in the smart contract");
+        if(!isOwner()) {
+            reduceAllowance(msg.sender, _amount);
+        }
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
     
-    fallback() external payable {}
+    /*
+    Annonymous function that allows you to deposit money 
+    to the smart contract
+    */
+    fallback() external payable {
+        emit MoneyReceived(msg.sender, msg.value);
+    }
 }
